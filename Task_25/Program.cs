4293203abd36c746c7e1852d68495198a5ca8081
Yuriy_Zmeev <yuriy.zmeev@mail.ru>
2022-10-14 11:34:37 +0600

    add Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,23 @@
+﻿int NameNumber(string massege)
+{
+    Console.WriteLine(massege);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int a = NameNumber("Введите число A: ");
+int b = NameNumber("Введите число B: ");
+
+int ExponentiationNumber(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+int exponentiation = ExponentiationNumber(a, b);
+Console.WriteLine("Ответ: " + exponentiation);
